fb7d7a3541e4e3f536db472239621e69bc7828fa
Amirhoseinsharifi <amirhoseinsharify90@gmail.com>
2025-12-13 22:10:32 +0330

    Add ShowcaseProduct Component

---
+++ app/home/components/ShowcaseProduct.tsx
@@ -0,0 +1,76 @@
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { Zoom, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css/pagination";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
+interface ShowProductProps {
+    title:string
+}
+
+export default function ShowcaseProduct({title}:ShowProductProps) {
+  const items = ["hello", "world", "foo", "bar"];
+
+  return (
+    <div className="m-20">
+      <h1 className="text-center text-2xl mb-10">{title}</h1>
+      <Swiper
+        modules={[Zoom, Pagination, Autoplay]}
+        spaceBetween={50}
+        breakpoints={{
+          0: { slidesPerView: 1, spaceBetween: 10 },
+          640: { slidesPerView: 2, spaceBetween: 20 },
+          1024: { slidesPerView: 3, spaceBetween: 30 },
+          1280: { slidesPerView: 4, spaceBetween: 40 },
+        }}
+        zoom
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 2000, pauseOnMouseEnter: true }}
+        className="!pb-15"
+      >
+        {items.map((item, id) => (
+          <SwiperSlide key={id}>
+            <Card >
+              <CardHeader>
+                <Image
+                  src={"/Hero.png"}
+                  width={300}
+                  height={200}
+                  alt="Hero Photo"
+                />
+              </CardHeader>
+              <CardContent>
+                <p>{item}</p>
+                <p className="truncate">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Perferendis, quasi? Eum, excepturi? Soluta recusandae tempore
+                  incidunt, nobis iure ab quia adipisci eveniet doloremque velit
+                  aspernatur, optio nihil pariatur, suscipit asperiores!
+                </p>
+              </CardContent>
+              <CardFooter className="flex justify-center">
+                <Button size="lg" className="bg-blue-500 hover:bg-blue-400">
+                  <FontAwesomeIcon icon={faCartShopping} size="lg" className="mb-1" />{" "}
+                  Buy Now
+                </Button>
+              </CardFooter>
+            </Card>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import ShowcaseProduct from './components/ShowcaseProduct'
 
 export default function page() {
   return (
     <div>
         <Hero/>
+        <ShowcaseProduct title='تخفیف خورده ها'/>
     </div>
   )
 }
